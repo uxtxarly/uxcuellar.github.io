79ad7d0d586b7e858aea30432290f2f978f80a03
Carlos Cuellar <uxcuellar@gmail.com>
2023-09-07 08:11:19 -0700

    make svg local

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./_includes/**/*.html", "./_layouts/**/*.html", "./blog/*.html", "./music/*.html", "./work/*.html","./_posts/*.html","./*.html"],
+  content: ["./_includes/**/*.html", "./work/a288d20020c6de3a6926698a5b7f0a39adce6c79/*.html","./_data/*.json", "./_layouts/**/*.html", "./blog/*.html", "./music/*.html", "./work/*.html","./_posts/*.html","./*.html"],
   theme: {
     extend: {
       fontFamily: {
